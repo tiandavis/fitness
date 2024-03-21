1a4a734881e3edbe6cc26c56f9f725e295a45d64
Tian Davis <tiandavis@gmail.com>
2024-03-20 21:13:32 -0400

    Adds Exercise Page Desktop and Mobile layout and styling.

--- pages/exercise/[slug].js
+++ pages/exercise/[slug].js
@@ -28,6 +28,38 @@ export default function ExercisePage() {
     <Layout title={exercise.name}>
       <div className={styles.article}>
         <img className={styles.image} src={exercise.image} alt={exercise.name} />
+
+        <div className={styles.details}>
+          <h2 className={styles["set-header"]}>Add set:</h2>
+          <label className={styles.label} htmlFor="reps">Reps</label>
+          <input className={styles.input} type="number" id="reps" name="reps" />
+
+          <label className={styles.label} htmlFor="weight">Weight</label>
+          <input className={styles.input} type="number" id="weight" name="weight" />
+
+          <button className={styles.button}>+</button>
+
+          <h2 className={styles["performance-header"]}>Performances:</h2>
+          <div className={styles.performances}>
+            <div className={styles.performance}>
+              <h3 className={styles.day}>03/23/2024</h3>
+              <div className={styles.sets}>
+                <div className={styles.set}>
+                  <div className={styles["reps-by-weight"]}>12 x 50 lb</div>
+                  <div className={styles["estimated-one-rep-max"]}><strong>Estimated 1RM:</strong> 80 lb</div>
+                </div>
+                <div className={styles.set}>
+                  <div className={styles["reps-by-weight"]}>12 x 50 lb</div>
+                  <div className={styles["estimated-one-rep-max"]}><strong>Estimated 1RM:</strong> 80 lb</div>
+                </div>
+                <div className={styles.set}>
+                  <div className={styles["reps-by-weight"]}>12 x 50 lb</div>
+                  <div className={styles["estimated-one-rep-max"]}><strong>Estimated 1RM:</strong> 80 lb</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </Layout>
   );
